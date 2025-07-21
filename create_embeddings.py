98395cf3771cbe63471fd99de6c2f89063d86202
YuS <31320755+YuSak1@users.noreply.github.com>
2025-07-21 23:28:18 +0200

    Fix FAISS index path and rename variable

--- create_embeddings.py
+++ create_embeddings.py
@@ -5,11 +5,11 @@ from langchain_community.vectorstores import FAISS
 import os
 import tqdm
 
-dir = "Transcripts"
+transcripts_dir = "Transcripts"
 texts = []
-for filename in tqdm.tqdm(os.listdir(dir)):
+for filename in tqdm.tqdm(os.listdir(transcripts_dir)):
     if filename.endswith(".txt"):
-        with open(os.path.join(dir, filename), "r", encoding="utf-8") as f:
+        with open(os.path.join(transcripts_dir, filename), "r", encoding="utf-8") as f:
             texts.append(Document(page_content=f.read()))
 
 splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=80)
@@ -18,4 +18,4 @@ split_docs = splitter.split_documents(texts)
 # Save FAISS
 embedding = OpenAIEmbeddings()
 db = FAISS.from_documents(split_docs, embedding)
-db.save_local("faiss_index_fore")
+db.save_local("faiss_index")
